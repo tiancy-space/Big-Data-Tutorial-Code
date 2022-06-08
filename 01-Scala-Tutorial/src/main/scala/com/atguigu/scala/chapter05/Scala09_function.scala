3425f4d174ccf2bb8e6fb5f65161cc0651d154df
tiancy <tiancy_01@163.com>
2022-06-08 16:41:03 +0800

    匿名函数的简化操作

--- 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter05/Scala09_function.scala
+++ 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter05/Scala09_function.scala
@@ -1,12 +1,18 @@
 package com.atguigu.scala.chapter05
 
-/**
-  * Scala - 函数式编程 - 函数作为值 - 匿名函数 - 至简原则
-  */
+/*
+  TODO Scala - 函数式编程 - 函数作为值 - 匿名函数 - 至简原则
+    1、匿名函数的声明: (参数列表) => {函数体}, eg:  (name:String,num:Int) => {name*num}
+    2、声明函数的参数列表类型为函数类型或者当前函数的返回值为函数类型. 函数类型的声明方式: (参数列表中每个参数的数据类型) => 当前函数的返回值类型
+
+ */
 object Scala09_function {
   def main(args: Array[String]): Unit = {
+
+    // 定义一个函数,函数的参数列表为一个函数类型,返回值为 Unit
     def fun ( f : String => String ) : Unit ={
-      println( f("zhangsan") )
+      // 函数内部,拿到当前fun函数的参数列表,是一个函数,并传入指定类型的参数.
+      println("匿名函数的使用一、" +  f("zhangsan") )
     }
 
     // 匿名函数
