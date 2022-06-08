8d34fa36de9e4f3dc9dcfc9c64dcc6b747142248
tiancy <tiancy_01@163.com>
2022-06-08 16:04:20 +0800

    Scala中函数作为 `函数`的返回值类型来使用.

--- 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter05/Scala07_function.scala
+++ 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter05/Scala07_function.scala
@@ -6,7 +6,7 @@ package com.atguigu.scala.chapter05
 object Scala07_function {
   def main(args: Array[String]): Unit = {
 
-    //1. 定义函数
+    //1. 定义函数fun(),其中它的返回值类型为 一个函数类型
     def fun() : String => String = {
       freturn
     }
@@ -31,6 +31,7 @@ object Scala07_function {
       freturn1
     }
 
+    // 函数调用,调用fun1()后,返回值是一个函数类型,其中返回的函数类型的参数为一个String,无返回值.
     fun1()("wangwu")
 
 
