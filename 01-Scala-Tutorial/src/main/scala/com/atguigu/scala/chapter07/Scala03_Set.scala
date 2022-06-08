c6634fbdae58802272f724cced6dba0df7cc8077
tiancy <tiancy_01@163.com>
2022-06-08 23:27:36 +0800

    Scala中Set集合的定义以及各种操作

--- 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter07/Scala03_Set.scala
+++ 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter07/Scala03_Set.scala
@@ -9,33 +9,33 @@ object Scala03_Set {
   def main(args: Array[String]): Unit = {
     //1. 不可变
     val set: Set[Int] = Set(1,2,3,4,5,6,5,6)
-    println(set)
+    println(set) // Set(5, 1, 6, 2, 3, 4)
 
-    println(set.getClass.getName)
+    println(set.getClass.getName) // scala.collection.immutable.HashSet$HashTrieSet
 
 
     //2. 可变
     val mset: mutable.Set[Int] = mutable.Set(1,2,3,4,5,6,5,6,7)
-    println(mset)
+    println(mset) //Set(1, 5, 2, 6, 3, 7, 4)
 
     //操作
     mset.add(8)
+    println(mset) // Set(1, 5, 2, 6, 3, 7, 4, 8)
+
+    mset.update(9,included = true)  // 如果列表中存在9是否操作.
+    println(mset) // Set(9, 1, 5, 2, 6, 3, 7, 4, 8)
+
+    mset.update(10,included = false )  //不操作
+    println(mset) // Set(9, 1, 5, 2, 6, 3, 7, 4, 8)
+
+    mset.update(1,true)  // 不操作 Set(9, 1, 5, 2, 6, 3, 7, 4, 8)
     println(mset)
 
-    mset.update(9,true)  // add
-    println(mset)
-
-    mset.update(10,false )  //不操作
-    println(mset)
-
-    mset.update(1,true)  // 不操作
-    println(mset)
-
-    mset.update(1,false)  //remove
+    mset.update(1,false)  //remove  Set(9, 5, 2, 6, 3, 7, 4, 8)
     println(mset)
 
     mset.remove(2)
-    println(mset)
+    println(mset) // Set(9, 5, 6, 3, 7, 4, 8)
 
     mset.remove(20)
 
