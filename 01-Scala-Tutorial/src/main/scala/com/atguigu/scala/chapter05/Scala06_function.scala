b9453107d1afa53924c470ec4c930293532aa6ea
tiancy <tiancy_01@163.com>
2022-06-08 15:48:17 +0800

    Scala中函数作为函数的参数类型来使用.在函数定义时,指定函数参数类型为一个匿名函数的形式 输入类型 => 返回值类型

--- 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter05/Scala06_function.scala
+++ 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter05/Scala06_function.scala
@@ -5,16 +5,18 @@ package com.atguigu.scala.chapter05
   */
 object Scala06_function {
   def main(args: Array[String]): Unit = {
-    //1. 定义函数
+    //1. 定义函数,当前函数的参数为 函数类型 [输入String,输出String]
     def fun( f : String =>String  ) : Unit = {
+      // 给当前传递进来的函数指定参数. 也就是调用函数执行.
       println(f("zhangsan"))
     }
 
-    //2. 给函数的参数传值
+    //2. 定义fun函数的参数:也是一个函数,其中具体的操作: 给定一个名称,将名称打印两遍
     def fparam( name : String ) : String = {
        name * 2
     }
-    fun(fparam)
+    // 真正的函数调用,其中函数的参数为一个函数,也就是我们上面写好的函数 fparam.
+    fun(fparam) // zhangsanzhangsan
 
 
     println("-------------------------------------")
@@ -27,7 +29,7 @@ object Scala06_function {
       println( name * 2 )
     }
 
-    fun1(fparam1)
+    fun1(fparam1) // lisilisi
   }
 
 }
