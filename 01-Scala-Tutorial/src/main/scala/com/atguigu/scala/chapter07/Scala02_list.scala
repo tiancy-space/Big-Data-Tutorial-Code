a7ebd8cd1f07bb05e44710dd651e4cbd10bf62b2
tiancy <tiancy_01@163.com>
2022-06-08 23:22:31 +0800

    Scala中列表的定义以及操作

--- 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter07/Scala02_list.scala
+++ 01-Scala-Tutorial/src/main/scala/com/atguigu/scala/chapter07/Scala02_list.scala
@@ -4,73 +4,74 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 /**
-  *  Scala - 集合 - list
-  */
+ * Scala - 集合 - list
+ */
 object Scala02_list {
   def main(args: Array[String]): Unit = {
     //1. 不可变list
-    val list = List[Int](1,2,3,4,5)
-    println(list)
-    // 操作
-    println(list(0))
-    println(list)
+    val list: List[Int] = List[Int](1, 2, 3, 4, 5)
+    println(list) // List(1, 2, 3, 4, 5)
+    // 操作 获取当前列表中的第一个元素.
+    println(list(0)) // 1
+    println(list.head) // 1
+    println(list) // List(1, 2, 3, 4, 5)
 
     // 符号操作
-    val str: String = list + "A"
-    println(str)
+    val str: String = list + "A" // List(1, 2, 3, 4, 5)A
 
     //val list1: List[Int] = list.:+(6)
+    // 给当不可变的列表添加元素,并形成一个新的列表,并将生成新的列表的地址给到 list1
     val list1: List[Int] = list :+ 6
-    println(list1)
+    println(list1) // List(1, 2, 3, 4, 5, 6)
 
     // 以：结尾的符号操作,是反向操作
     //val list2: List[Int] = list.+:(6)
     val list2: List[Int] = 6 +: list
-    println(list2)
+    println(list2) // // List(6, 1, 2, 3, 4, 5)
 
-    val list3: List[Int] = 5::6::7::8::list
-    println(list3)
-    println(Nil)
+    val list3: List[Int] = 5 :: 6 :: 7 :: 8 :: list
+    println(list3) // List(5, 6, 7, 8, 1, 2, 3, 4, 5)
+    println(Nil) // List()
 
-    val list4: List[Int] = 1::2::3::4::Nil
-    println(list4)
+    val list4: List[Int] = 1 :: 2 :: 3 :: 4 :: Nil
+    println(list4) // List(1, 2, 3, 4)
 
-    val list5: List[Any] = 5::6::7::8::list::Nil
-    println(list5)
+    val list5: List[Any] = 5 :: 6 :: 7 :: 8 :: list :: Nil
+    println(list5) // List(5, 6, 7, 8, List(1, 2, 3, 4, 5))
 
-    val list6: List[Any] = 5::6::7::8::list:::Nil
-    println(list6)
+    val list6: List[Any] = 5 :: 6 :: 7 :: 8 :: list ::: Nil
+    println(list6) //  List(5, 6, 7, 8, 1, 2, 3, 4, 5)
 
     println("----------------------------------------")
     //2. 可变list
 
     //val listBuffer = new ListBuffer[Int]()
 
-    val listBuffer: ListBuffer[Int] = ListBuffer(1,2,3,4,5)
+    val listBuffer: ListBuffer[Int] = ListBuffer(1, 2, 3, 4, 5)
 
-    println(listBuffer)
+    println(listBuffer) // ListBuffer(1, 2, 3, 4, 5)
 
     //操作
-    listBuffer.append(6,7,8)
+    listBuffer.append(6, 7, 8) // ListBuffer(1, 2, 3, 4, 5, 6, 7, 8)
     println(listBuffer)
 
-    listBuffer.insert(0,10,11,12)
+    listBuffer.insert(0, 10, 11, 12) // ListBuffer(10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8)
     println(listBuffer)
 
     listBuffer.remove(0)
+    println(listBuffer) // ListBuffer(11, 12, 1, 2, 3, 4, 5, 6, 7, 8)
+
+    listBuffer.remove(0, 2) // ListBuffer(1, 2, 3, 4, 5, 6, 7, 8)
     println(listBuffer)
 
-    listBuffer.remove(0,2)
-    println(listBuffer)
-
-    listBuffer.update(0,0)
-    println(listBuffer)
+    listBuffer.update(0, 0)
+    println(listBuffer) // ListBuffer(0, 2, 3, 4, 5, 6, 7, 8)
 
     //3. 可变  不可变 转换
 
-    val list7 = List(1,2,3)
+    val list7 = List(1, 2, 3)
 
-    val listBuffer1: ListBuffer[Int] = ListBuffer(5,6,7)
+    val listBuffer1: ListBuffer[Int] = ListBuffer(5, 6, 7)
 
     // 可变 -> 不可变
 
